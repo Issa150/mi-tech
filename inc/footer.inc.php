3be56a2e52c7e96497bb897b428b8d6093a7b846
Issa <ebrahimhedari76@gmail.com>
2024-03-26 16:58:39 +0100

    section is done!

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,6 +1,38 @@
-    
+</div>
+<footer>
+
+    <div class="flex-row">
+        <ul>
+            <h3>Resources</h3>
+            <li>Contacter nous</li>
+            <li>S.A.V</li>
+            <li>Informations Légales</li>
+        </ul>
+        <ul>
+            <h3>Besoin d'aide?</h3>
+            <li>Une question ? </li>
+            <li>Contactez-nous </li>
+            <li>Retour et remboursement</li>
+        </ul>
+        <ul>
+            <h3>Shopping en ligne facile</h3>
+            <li>Essayez d'abord, payez après</li>
+            <li>S'inscrire à la newsletter</li>
+            <li>Suivre mon colis</li>
+        </ul>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="<?=RACINE_SITE?>assets/js/script.js"></script>
+
+    <div class="row-contact">
+        <h3>Visiter notre magasin à Paris . Lorem ipsum isha fksnvhep fhahandf.</h3>
+        <div class="search-container">
+            <input type="search" name="search" id="search" placeholder="Recherchez par Marque, modele ...">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+    </div>
+
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="<?= RACINE_SITE ?>assets/js/script.js"></script>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,29 +1,103 @@
-<?php 
+<?php
 include_once "config/function.config.php";
 
 $title = 'Accueil';
-include_once "inc/header.inc.php";?>
+include_once "inc/header.inc.php"; ?>
 
 
 <header>
     <!-- Swiper -->
-  <div class="swiper mySwiper">
-    <div class="swiper-wrapper">
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
 
-      <div class="swiper-slide">
-        <img src="assets/imgs/1_720x405_V1.avif" alt="">
-        
+            <div class="swiper-slide">
+                <img src="assets/imgs/1_720x405_V1.avif" alt="">
+                <!-- <div class="context">
+            <h2>2024 <br> 12.5% Remise</h2>
+            <button>Découvréz</button>
+
+        </div> -->
+            </div>
+            <div class="swiper-slide">Slide 2</div>
+        </div>
+
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
     </div>
-    
-      <div class="swiper-slide">Slide 2</div>
-    </div>
-    
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-pagination"></div>
-  </div>
 </header>
 
+<section>
+    <h2>Nos Offres</h2>
+    <div class="section-main featured">
+        <ul>
+            <li class="active">Téléphones</li>
+            <li>Ordinateurs</li>
+            <li>Accessoires</li>
+        </ul>
+        <div class="grid-row">
+            <img src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1672854207187-e70df893755b?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1629317297801-8ba848bec2ce?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1566647387313-9fda80664848?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1551739440-5dd934d3a94a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        </div>
+    </div>
+</section>
+
+<section>
+    <h2>Téléphones</h2>
+    <div class="section-main">
+        <ul>
+            <li class="active">Téléphones</li>
+            <li>Ordinateurs</li>
+            <li>Accessoires</li>
+        </ul>
+        <div class="grid-row">
+            <img src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1672854207187-e70df893755b?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1629317297801-8ba848bec2ce?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1566647387313-9fda80664848?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1551739440-5dd934d3a94a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        </div>
+    </div>
+</section>
+
+<section>
+    <h2>Ordinateus</h2>
+    <div class="section-main">
+        <ul>
+            <li class="active">Téléphones</li>
+            <li>Ordinateurs</li>
+            <li>Accessoires</li>
+        </ul>
+        <div class="grid-row">
+            <img src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1672854207187-e70df893755b?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1629317297801-8ba848bec2ce?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1566647387313-9fda80664848?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1551739440-5dd934d3a94a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        </div>
+    </div>
+</section>
+
+<section>
+    <h2>Accessoires</h2>
+    <div class="section-main">
+        <ul>
+            <li class="active">Téléphones</li>
+            <li>Ordinateurs</li>
+            <li>Accessoires</li>
+        </ul>
+        <div class="grid-row">
+            <img src="https://plus.unsplash.com/premium_photo-1711051475117-f3a4d3ff6778?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1672854207187-e70df893755b?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1629317297801-8ba848bec2ce?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1566647387313-9fda80664848?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <img src="https://images.unsplash.com/photo-1551739440-5dd934d3a94a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        </div>
+    </div>
+</section>
 
 
 
@@ -31,9 +105,4 @@ include_once "inc/header.inc.php";?>
 
 
 
-
-
-
-
-
-<?php include_once "inc/footer.inc.php"?>
+<?php include_once "inc/footer.inc.php" ?>
